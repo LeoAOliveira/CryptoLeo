d3914ced1efff997bd577e76afe5c6c208868006
Leonardo Amorim de Oliveira <leonardo.aoliveira@icloud.com>
2021-09-28 00:09:48 -0300

    Create Transaction model

---
+++ Crypto Leo/Crypto Leo/Models/Transaction.swift
@@ -0,0 +1,14 @@
+//
+//  Transaction.swift
+//  Crypto Leo
+//
+//  Created by Leonardo Oliveira on 28/09/21.
+//
+
+import Foundation
+
+struct Transaction {
+    let receiver: String
+    let sender: String
+    let amount: Double
+}
