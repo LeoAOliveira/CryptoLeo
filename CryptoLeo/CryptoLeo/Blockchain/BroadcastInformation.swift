5e3ae2faf0ed95aa79c173e55c1b6501a12aff86
Leonardo Amorim de Oliveira <leonardo.aoliveira@icloud.com>
2021-10-09 16:58:23 -0300

    Add documentation to BroadcastInformation

--- CryptoLeo/CryptoLeo/Blockchain/BroadcastInformation.swift
+++ CryptoLeo/CryptoLeo/Blockchain/BroadcastInformation.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+/// Possible informations to be broadcasted to multi-peer session.
+///
+/// The enum contains:
+/// - Current `Blockchain`.
+/// - New `Block` recently mined and added to blockchain.
+/// - Newt `Transaction` available for mining.
 enum BroadcastInformation {
+    
+    /// Currently stored `Blockchain`.
     case currentBlockchain(Blockchain)
+    
+    /// Recently mined and added `Block`.
     case newBlock(Block)
+    
+    /// Recently created `Transaction`.
     case newTransaction(Transaction)
 }
